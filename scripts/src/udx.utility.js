84258cc1c09071273d8611cb6b40075e1df44986
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-01-18 07:27:50 -0500

    Automated commit.

--- scripts/src/udx.utility.js
+++ scripts/src/udx.utility.js
@@ -5,9 +5,43 @@
  * @returns {Object}
  */
 define( 'udx.utility', function( require, exports, module ) {
-  console.log( module.id, 'loaded' );
+  console.debug( module.id, 'loaded' );
 
   return {
+
+    /**
+     * Deep Extend Object.
+     *
+     * @param destination
+     * @param source
+     * @returns {*}
+     */
+    extend: function extend( destination, source ) {
+      console.debug( 'udx.utility', 'extend' );
+
+      for( var property in source ) {
+        if( source[property] && source[property].constructor && source[property].constructor === Object ) {
+          destination[property] = destination[property] || {};
+          arguments.callee( destination[property], source[property] );
+        } else {
+          destination[property] = source[property];
+        }
+      }
+      return destination;
+    },
+
+    /**
+     * Extend Defaults into Object
+     *
+     * @param str
+     * @return
+     */
+    defaults: function( target, defaults ) {
+      console.debug( 'udx.utility', 'defaults' );
+
+      return this.extend( defaults || {}, target || {} );
+
+    },
 
     /**
      * Create Slug from String.
@@ -16,10 +50,11 @@ define( 'udx.utility', function( require, exports, module ) {
      * @return
      */
     create_slug: function create_slug( str ) {
+      console.debug( 'udx.utility', 'create_slug' );
+
       return str.replace( /[^a-zA-Z0-9_\s]/g, "" ).toLowerCase().replace( /\s/g, '_' );
     }
 
   }
-
 
 });
--- scripts/src/udx.utility.process.js
+++ scripts/src/udx.utility.process.js
@@ -15,24 +15,57 @@
  *
  * @version 0.1.0
  */
-define( 'udx.utility.process', [ 'async' ], function() {
-  console.log( 'udx.utility.process loaded' );
+define( 'udx.utility.process', [ 'udx.utility', 'async', 'jquery' ], function() {
+  console.debug( 'udx.utility.process', 'loaded' );
 
   // Modules.
-  var Auto    = require( 'async' ).auto;
-  var Series  = require( 'async' ).series;
-
-  // Module Properties.
-  var _processes = {};
+  var Auto      = require( 'async' ).auto;
+  var Series    = require( 'async' ).series;
+  var Utility   = require( 'udx.utility' );
 
   /**
    * Process Instance
    *
+   * @param args              {Object}
+   * @param args.id           {String}
+   * @param args.ajax         {String}
+   * @param args.args         {Object}
+   * @param args.poll         {Number}
+   * @param args.timeout      {Number}
+   * @param args.protocol     {String}
+   * @param args.format       {String}
+   * @param args.headers      {Object}
+   * @param args.onStart      {Function}
+   * @param args.onComplete   {Function}
+   * @param args.onError      {Function}
+   * @param args.onTimeout    {Function}
+   *
+   * @returns {Process}
+   * @constructor
    */
-  function Process() {
-    console.log( 'new Process' );
+  function Process( args ) {
+    console.debug( 'udx.utility.process', 'new Process' );
 
-    return this;
+    // Configure Settings.
+    this.settings = Utility.defaults( args, {
+      id: null,
+      ajax: null,
+      args: {},
+      poll: 5000,
+      timeout: 10000,
+      protocol: 'ajax',
+      format: 'json',
+      headers: {},
+      onStart: function onStart( error, data ) {},
+      onComplete: function onComplete( error, data ) {},
+      onError: function onError( error, data ) {},
+      onTimeout: function onTimeout( error, data ) {}
+    });
+
+    //console.debug( 'process.settings', this.settings );
+
+    // @chainable
+    return Process.instances[ args.id ] = this;
 
   }
 
@@ -41,23 +74,84 @@ define( 'udx.utility.process', [ 'async' ], function() {
    *
    */
   Object.defineProperties( Process.prototype, {
-    auto: {
+    start: {
       /**
-       * Auto Task Runner.
+       * Start Process.
        *
        */
-      value: require( 'async' ).auto,
+      value: function startProcess() {
+        console.debug( 'udx.utility.process', 'startProcess' );
+
+        var _context = this;
+
+        jQuery.ajax({
+          url: _context.settings.ajax,
+          timeout: _context.settings.timeout,
+          async: true,
+          cache: false,
+          type: 'GET',
+          contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+          dataType: _context.settings.format || 'json',
+          headers: _context.settings.headers || {},
+          data: Utility.extend({
+            event: 'start-process',
+            id: _context.settings.id,
+            type: _context.settings.type
+          }, _context.settings.args ),
+          beforeSend: function beforeSend() {
+            // console.debug( 'udx.utility.process', 'beforeSend', arguments );
+          },
+          error: function error( error ) {
+            console.debug( 'udx.utility.process', 'error', arguments );
+
+            _context.settings.onStart( new Error( 'Process Start Error: ' + error ) );
+
+          },
+          complete: function complete( response, status ) {
+            console.debug( 'udx.utility.process', 'complete', status );
+
+            if( response.responseJSON ) {
+              _context.settings.onStart( null, response.responseJSON );
+            }
+
+          }
+        });
+
+        // @chainable
+        return this;
+
+      },
       enumerable: true,
       configurable: true,
       writable: true
     },
-    testMethod: {
+    poll: {
       /**
-       * Test Prototypal Method.
+       * Poll Started Process.
        *
        */
-      value: function testMethod() {
-        console.log( 'testMethod' );
+      value: function pollProcess() {
+        console.debug( 'udx.utility.process', 'pollProcess' );
+
+        // @chainable
+        return this;
+
+      },
+      enumerable: true,
+      configurable: true,
+      writable: true
+    },
+    recover: {
+      /**
+       * Poll Started Process.
+       *
+       */
+      value: function recoverProcess() {
+        console.debug( 'udx.utility.process', 'recoverProcess' );
+
+        // @chainable
+        return this;
+
       },
       enumerable: true,
       configurable: true,
@@ -80,6 +174,16 @@ define( 'udx.utility.process', [ 'async' ], function() {
         return new Process( arguments[0] );
       },
       enumerable: true,
+      configurable: true,
+      writable: true
+    },
+    instances: {
+      /**
+       * Track Instances.
+       *
+       */
+      value: {},
+      enumerable: false,
       configurable: true,
       writable: true
     }
