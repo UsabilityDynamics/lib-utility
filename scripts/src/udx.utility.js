e2f445e0ebc5c5f2b958c4a0a7b0d1d58e010830
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-01-20 18:23:50 -0500

    Automated commit.

--- scripts/src/udx.utility.js
+++ scripts/src/udx.utility.js
@@ -4,10 +4,73 @@
  * @version 0.2.3
  * @returns {Object}
  */
-define( 'udx.utility', function( require, exports, module ) {
+define( 'udx.utility', function Utility( require, exports, module ) {
   // console.debug( module.id, 'loaded' );
 
   return {
+
+    /**
+     * HTTP GET Request
+     *
+     * The callback method follows Node.js format of returning an Error object as the first argument, or null.
+     *
+     * @method remote_get
+     * @for Utility
+     *
+     * @param url {String|Object}
+     * @param callback {Function}
+     */
+    remote_get: function remote_get() {
+      var request = makeHttpObject();
+
+      var url = arguments[0];
+      var callback = arguments[1];
+
+      request.open( 'GET' , url, true);
+
+      request.send( null );
+
+      request.onreadystatechange = function() {
+
+        if (request.readyState == 4) {
+          if (request.status == 200) {
+            callback( null, request.responseText );
+          } else if (failure) {
+            callback( new Error( request.statusText) );
+          }
+        }
+
+      };
+
+    },
+
+    /**
+     * HTTP POST Request
+     *
+     */
+    remote_post: function remote_post() {
+      var request = makeHttpObject();
+
+      var url = arguments[0];
+      var callback = arguments[1];
+
+      request.open( 'POST' , url, true);
+
+      request.send( null );
+
+      request.onreadystatechange = function() {
+
+        if (request.readyState == 4) {
+          if (request.status == 200) {
+            callback( null, request.responseText );
+          } else if (failure) {
+            callback( new Error( request.statusText) );
+          }
+        }
+
+      };
+
+    },
 
     /**
      * Deep Extend Object.
