0f8c5955c8a59a37c9db7196be983037a9e193f7
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-01-21 20:03:09 +0200

    Added parse_str() method

--- lib/class-utility.php
+++ lib/class-utility.php
@@ -105,12 +105,35 @@ namespace UsabilityDynamics {
        *
        * @return object
        */
-      public static function parse_args( $args, $defaults ) {
+      static public function parse_args( $args, $defaults ) {
 
         return (object) wp_parse_args( $args, $defaults );
 
       }
 
+      /**
+       * Parses Query.
+       * HACK. The current logic solves the issue of max_input_vars in the case if query is huge.
+       * For example, user can set more than 150 property attributes (WP-Property) where every attribute has own set of params.
+       * 
+       * @see parse_str() Default PHP function
+       * @version 1.0
+       * @author peshkov@UD
+       */
+      static public function parse_str( $request, $data = array() ) {
+        $hash = md5( '%2B' );
+        $request = str_replace( '%2B', $hash, $request );
+        $request = urldecode( $request );
+        $request = str_replace( $hash, '%2B', $request );
+        $tokens = explode( "&", $request );
+        foreach ( $tokens as $token ) {
+          $arr = array();
+          parse_str( $token, $arr );
+          $data = self::extend( $data, $arr );
+        }
+        return $data;
+      }
+      
       /**
        * Deep Conversion
        *
